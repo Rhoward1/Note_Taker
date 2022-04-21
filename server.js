dff3f93db90b4198399f7f1662b620cfd243672f
Rhoward1 <ryanmhoward@gmail.com>
2022-04-20 23:10:30 -0500

    finished file and folder structure. and worked on server.js

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+
+
+
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+
+
+app.use(apiRoutes)
+app.use(htmlRoutes)
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+
+// // GET Route for feedback page
+// app.get('/feedback', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
